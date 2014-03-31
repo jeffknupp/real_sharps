83ffc42e3eb4f6020e56229ff7ae8e3cd25727a7
Jeff Knupp <jknupp@appnexus.com>
2014-03-31 08:38:49 -0400

    move pybb templates in; change size of front image; add breadcrumb support

---
+++ scripts/populate.py
@@ -0,0 +1,8 @@
+from cappers.models import Sport
+
+Sport.objects.create(name='NFL')
+Sport.objects.create(name='NBA')
+Sport.objects.create(name='MLB')
+Sport.objects.create(name='NHL')
+Sport.objects.create(name='NCAAF')
+Sport.objects.create(name='NCAAMB')
